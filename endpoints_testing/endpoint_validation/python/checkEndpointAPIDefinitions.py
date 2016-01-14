d7fb1734ad179ec49fa561596e277a24e6f5287a
Michael Miller <mmiller@systemsbiology.org>
2016-01-13 16:12:04 -0800

    implemented first pass at a report on theendpoints

--- endpoints_testing/endpoint_validation/python/checkEndpointAPIDefinitions.py
+++ endpoints_testing/endpoint_validation/python/checkEndpointAPIDefinitions.py
@@ -15,8 +15,6 @@ import traceback
 def create_log(log_dir, log_name):
     try:
         # creates the logs for the run
-        if not os.path.isdir(log_dir):
-            os.makedirs(log_dir)
         logger = logging.getLogger(log_name)
         logger.setLevel(logging.DEBUG)
         log = logging.FileHandler(log_dir + log_name + '.txt', 'w')
@@ -65,15 +63,77 @@ def main(configFileName, website):
         raise e
     log.info('%s verify API against current for %s' % (datetime.now(), website))
 
-    # Create resource_manager client and loop over the project names
+    # make sure output directory exists
+    if not os.path.isdir(config['outdir']):
+        os.makedirs(config['outdir'])
+    # Discory the endpoints and get information on each one
     api_list = get_endpoints_list(website, config, log)
     save2file(config, log, api_list, 'discovery')
-    for item in api_list['items']:
-        endpoint = item['name']
-        version = item['version']
-        endpoint_info = get_endpoints_methods(website, endpoint, version, config, log)
-        save2file(config, log, endpoint_info, endpoint + '.' + version)
+    report = config['endpoint_report']
+    endpoint_keys = report['endpoint_def']['columns']
+    with open(config['outdir'] + report['filename'], 'w') as reportfile:
+        reportfile.write(report['header'] % (website) + '\n')
+        for item in api_list['items']:
+            endpoint = item['name']
+            version = item['version']
+            endpoint_info = get_endpoints_methods(website, endpoint, version, config, log)
+            save2file(config, log, endpoint_info, endpoint + '.' + version)
     
+            # get the top-level information on the endpoint from the discovery document
+            endpoint_body = '\t'
+            for key in endpoint_keys:
+                endpoint_body += item[key] + '\t'
+            endpoint_body += '\n'
+            # and the more fine grained detailed information form the endpoint specific document
+            top_resources = endpoint_info['resources']
+            for named_endpoints in top_resources.itervalues():
+                if 'resources' not in named_endpoints:
+                    continue
+                resources = named_endpoints['resources']
+                for key, endpoint in resources.iteritems():
+                    endpoint_body += '\t\t' + key + '\n'
+                    for key, method in endpoint['methods'].iteritems():
+                        endpoint_body += '\t\t\t' + key + '\n\t\t\t\t' + method['path'] + '\t' + method['id'] + '\t' + method['httpMethod'] + '\t' + ','.join(method['scopes']) + '\n'
+                        if 'parameters' not in method:
+                            continue
+                        endpoint_body += '\t\t\t\tparameters:\n'
+                        parameters = method['parameters']
+                        if 'parameterOrder' in method:
+                            parameterOrder = method['parameterOrder']
+                            for paramname in parameterOrder:
+                                parameter = parameters.pop(paramname)
+                                endpoint_body += '\t\t\t\t\t' + paramname + '\t' + parameter['type'] + '\trequired: ' + ('true' if 'required' in parameter and parameter['required'] else 'false') + '\n'
+                        for paramname in parameters:
+                            parameter = parameters[paramname]
+                            endpoint_body += '\t\t\t\t\t' + paramname + '\t' + parameter['type'] + '\trequired: ' + ('true' if 'required' in parameter and parameter['required'] else 'false') + '\n'
+            reportfile.write(endpoint_body + '\n')
+
+#       "delete": {
+#        "id": "cohort_api.cohort_endpoints.cohort.delete",
+#        "path": "delete_cohort",
+#        "httpMethod": "POST",
+#        "parameters": {
+#         "cohort_id": {
+#          "type": "string",
+#          "required": true,
+#          "format": "int64",
+#          "location": "query"
+#         },
+#         "token": {
+#          "type": "string",
+#          "location": "query"
+#         }
+#        },
+#        "parameterOrder": [
+#         "cohort_id"
+#        ],
+#        "response": {
+#         "$ref": "ApiCohortReturnJSON"
+#        },
+#        "scopes": [
+#         "https://www.googleapis.com/auth/userinfo.email"
+#        ]
+#       },
     log.info(str(datetime.now()) + ' finished verifying API against current for %s' % (website))
 
 if __name__ == '__main__':
