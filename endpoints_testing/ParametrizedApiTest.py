442568dcd4785de6b1d0af7e15fefde190e01a1e
Abigail Hahn <Abigail.Hahn@systemsbiology.org>
2016-01-15 16:53:50 -0800

    Mostly finished the initial implementation of the unit test classes for endpoints

--- endpoints_testing/ParametrizedApiTest.py
+++ endpoints_testing/ParametrizedApiTest.py
@@ -1,27 +1,20 @@
 import unittest
 
 class ParametrizedApiTest(unittest.TestCase):
-	def __init__(self, methodName="runTest", api=None, version=None, resource=None, test_method_name=None, crud_op=None, create_method_name=None, delete_method_name=None, delete_key=None, config=None, config_dir=None, num_requests=None, auth=None):
+	def __init__(self, methodName="runTest", api=None, version=None, config=None, num_requests=None, auth=None):
 		super(ParametrizedTestCase, self).__init__(methodName)
 		self.api = api
-		self.version = version
-		self.resource = resource
-		self.test_method_name = test_method_name
-		self.crud_op = crud_op
-		self.delete_method_name = delete_method_name
-		self.delete_key = delete_key
-		self.config = config # this is the "type" of configuration for the test (one of "minimal", "missing_required_params", "optional_params", "undefined_param_names", "undefined_param_values", "incorrect_param_types")
-		self.config_dir = config_dir # this is the directory location of the requests (json files) which match the given configuration
-		self.num_requests = num_requests # the number of requests to make simultaneously for the test
-		self.auth = auth # will be a dictionary containing a username/password combo -- do I need to use a copy() method for this?
+		self.config = config  
+		self.num_requests = num_requests 
+		self.auth = auth 
 		
 	@staticmethod
-	def parametrize(testcase_class, api=None, version=None, resource=None, test_method_name=None, crud_op=None, delete_method_name=None, delete_key=None, config=None, config_dir=None, num_requests=None, auth=None):
+	def parametrize(testcase_class, api=None, version=None, config=None, num_requests=None, auth=None):
 		testloader = unittest.TestLoader()
 		testnames = testloader.getTestCaseNames(testcase_class)
 		suite = unittest.TestSuite()
 		for name in testnames:
-			suite.addTest(testcase_class(name, api=api, version=version, resource=resource, test_method_name=test_method_name, crud_op=crud_op, delete_method_name=delete_method_name, delete_key=delete_key, config=config, config_dir=config_dir, num_requests=num_requests, auth=auth))
+			suite.addTest(testcase_class(name, api=api, version=version, resource=resource, test_method_name=test_method_name, crud_op=crud_op, delete_method_name=delete_method_name, delete_key=delete_key, config=config, num_requests=num_requests, auth=auth))
 		return suite
 		
 		# NOTES: 
@@ -30,11 +23,10 @@ class ParametrizedApiTest(unittest.TestCase):
 		# version: the api version to test (e.g., "v1")
 		# resource: the name of the resource under which to choose methods to run (e.g., "cohort")
 		# test_method_name: the name of the endpoint method to test (e.g., "save")
-		# crud_op: the CRUD operation (CREATE, READ, UPDATE, DELETE) for the test method
+		# crud_op: the CRUD operation (CREATE, READ, UPDATE, DELETE) of the method being tested
 		# create_method_name: the name of the create method for an item; only required if crud_op=DELETE
 		# delete_method_name: the name of the delete method for an item created during the test; only required if crud_op=CREATE
 		# delete_key: a parameter to use to delete created objects during the teardown phase; only required if crud_op=CREATE
 		# config: the test configuration type (one of "minimal", "missing_required_params", "optional_params", "undefined_param_names", "undefined_param_values", "incorrect_param_types", "incorrect_permissions")
-		# config_dir: the directory containing the JSON formatted config files for each test
 		# num_requests: the number of concurrent requests to run -- to use for load testing; default=1
 		# auth: a dict containing user credentials for logging in using selenium (e.g., { "username":"...", "password": "..." } )
--- endpoints_testing/TestCohortApi.py
+++ endpoints_testing/TestCohortApi.py
@@ -1,13 +1,14 @@
 import os
+import sys
 import time
 import json
-import random
 import pprint
 import httplib2
 import isb_auth
 import isb_curl
 import requests 
 import unittest
+from copy import deepcopy
 from multiprocessing import Pool
 from apiclient import discovery
 from jsonspec.reference import resolve
@@ -45,6 +46,18 @@ class IsbCgcApiTest(ParametrizedApiTest):
 	def setUp(self):
 		# need to keep track of any items that were created by the test so they can be deleted later
 		self.created_items = []
+
+		# select a group (num_requests many) of requests from the given config
+		json_config = json.load("config/{api}/{config_type}/{method}.json".format(api=self.api, config_type=self.config, method=self.method))
+		
+		self.resource = json_config["resource"]
+		self.method = json_config["resource_test_method"]
+		self.crud_op = json_config["resource_crud_op"]
+		self.create_method = json_config["resource_create_method"]
+		self.delete_method = json_config["resource_delete_method"]
+		self.delete_key = json_config["resource_delete_key"]
+		self.requires_auth = json_config["requires_auth"]
+		self.requests_params = json_config["resource_requests"][0:self.num_requests]
 			
 		# authenticate (or not, depending on whether the "auth" dict contains an entry or is None)
 		if self.auth is not None:
@@ -56,14 +69,29 @@ class IsbCgcApiTest(ParametrizedApiTest):
 		http = httplib2.Http()
 		http = credentials.authorize(http)
 		
-		# build a an API service object and get information about the endpoint method to test
 		api_root = 'https://mvm-dot-isb-cgc.appspot.com/_ah/api'
-		discovery_url = '%s/discovery/v1/apis/%s/%s/rest' % (api_root, "{api}_api".format(api=self.api), self.version) # consider refactoring api and version to ParametrizedApiTest
-		discovery_doc = resolve(requests.get(discovery_url).json())
-		method_info = discovery_doc["resources"]["{api}_endpoints".format(api=self.api)]["resources"]["{resource}".format(resource=self.resource)]["methods"]["{method}".format(self.method)]
+		discovery_url = '%s/discovery/v1/apis/%s/%s/rest' % (api_root, "{api}_api".format(api=self.api), self.version)
+		discovery_doc = requests.get(discovery_url).json()
 		self.service = discovery.build(
-      self.api, self.version, discoveryServiceUrl=discovery_url, http=http)
-		
+      self.api, self.version, discoveryServiceUrl=discovery_url, http=http)		
+
+		# get information about the endpoint method to test
+		def resolve_json_document_references(document): # this could probably be moved to a utils module, since it should be general enough to be used anywhere
+			resolved_document = deepcopy(document)
+			for key, value in document.iteritems():
+				if type(value) is dict and "$ref" in value.keys():
+					new_value = resolve(document, "#/{key}".format(key=key))
+
+				else:
+					new_value = resolve_json_document_references(value)	
+				
+				resolved_document[key] = new_value
+
+			return resolved_document
+
+		resolved_disovery_doc = resolve_json_document_references(discovery_doc)
+		method_info = resolved_discovery_doc["resources"]["{api}_endpoints".format(api=self.api)]["resources"]["{resource}".format(resource=self.resource)]["methods"]["{method}".format(self.method)]
+
 		# get expected response
 		self.response_body_dict = {}
 		expected_response = method_info["response"]
@@ -71,125 +99,137 @@ class IsbCgcApiTest(ParametrizedApiTest):
 			self.response_body_dict[property_name] = property_attr["type"]
 
 		# set up the test
-		self.json_requests = []
 		self.process_pool = Pool(self.num_requests)
 		method_grandparent = getattr(self.service, "{api}_endpoints".format(api=self.api))
 		method_parent = getattr(method_grandparent, "{resource}".format(resource=self.resoure))
-		self.method_to_call = getattr(method_parent, "{method}".format(self.method))
-		
-		# select a random group (num_requests many) of files containing cohort requests from the given config directory
-		# note to self: may need to update the format of the config files
-		all_files = [os.listdir(self.config_dir)]
-		count = 0
-		while count < self.num_requests:
-			next_file = random.choice(all_files)
-			self.json_requests.append(json.load(next_file))
-			all_files.pop(next_file)
-			count += 1
+		self.method_to_call = getattr(method_parent, "{method}".format(method=self.method))
+		self.delete_method = getattr(method_parent, "{delete_method}".format(delete_method=self.delete_method_name)
 			
 		# if the operation type is "DELETE", create some items and store the item ids in the created_items array
 		if self.crud_op == "DELETE":
+			pass
 			
 		
 	def tearDown(self):
 		# delete items if the setUp authenticated a user (auth is not None) or if the created items array contains anything
 		if self.auth is not None or if len(self.created_items) > 0:
-			method_grandparent = getattr(self.service, "{api}_endpoints".format(api=self.api))
-			method_parent = getattr(method_grandparent, "{resource}".format(resource=self.resoure))
-			method_to_call = getattr(method_parent, self.delete_method_name)
 			for item in self.created_items:
 				item_to_delete = item[self.delete_key]
-				response = method_to_call(item_to_delete)
+				response = self.delete_method(item_to_delete)
 				
 	@skipIfUnauthenticated
-	@skipIfConfigMismatch("minimal")
 	def test_authenticated(self): # use for load testing
-		start = time.time()
-		results = self.process_pool.map(self.method_to_call, self.json_requests)
-		end = time.time()
-		execution_time = end - start
+		results, execution_time = self._query_endpoint()
 		# log execution time
 		for r in results:
-			# make an assertion about the status code
-			for key, value in self.response_body_dict.iteritems():
-				self.assertIn(key,r.keys())
-				self.assertIs(type(r[key]), value)
-			if self.operation_type == "CREATE":
-				self.created_items.append(r)
+			self.assertEqual(r.status_code, 200)
+			self._check_response(r)
+			
 	
 	@skipIfAuthenticated
 	def test_unauthenticated(self): 
-		pass
-		
-	@skipIfUnauthenticated
-	@skipIfConfigMismatch("incorrect_permissions")
-	def test_incorrect_permissions(self):
-		pass
+		results, execution_time = self._query_endpoint()
+		# log execution time
+		for r in results:
+			if self.requires_auth:
+				self.assertEqual(r.status_code, 401)
+			else:
+				self.assertEqual(r.status_code, 200)
+				self._check_response(r)
 	
 	@skipIfUnauthenticated
-	@skipIfConfigMismatch("missing_required_params")
-	def test_missing_required_params(self):
-		pass
-	
-	@skipIfUnauthenticated
-	@skipIfConfigMismatch("optional_params")
-	def test_optional_params(self): 
-		pass
-	
-	@skipIfUnauthenticated
-	@skipIfConfigMismatch("undefined_param_values")
-	def test_undefined_param_values(self): 
-		pass
+	@skipIfConfigMismatch("incorrect_param_types") # parameter values given are the wrong type
+	def test_incorrect_param_types(self):
+		results, execution_time = self._query_endpoint()
+		# log execution time
+		for r in results:
+			self.assertEqual(r.status_code, 400)
 
 	@skipIfUnauthenticated
-	@skipIfConfigMismatch("undefined_param_names")
-	def test_undefined_param_names(self):
-		pass
+	@skipIfConfigMismatch("incorrect_param_values") # parameter values given are the wrong format
+	def test_incorrect_param_values(self):
+		results, execution_time = self._query_endpoint()
+		# log execution time
+		for r in results:
+			self.assertEqual(r.status_code, 400)
+
+	@skipIfUnauthenticated
+	@skipIfConfigMismatch("incorrect_permissions") # the user doesn't have the correct permissions -- is this even testable in for our apis?
+	def test_incorrect_permissions(self):
+		results, execution_time = self._query_endpoint()
+		# log execution time
+		for r in results:
+			self.assertEqual(r.status_code, 401)
 	
 	@skipIfUnauthenticated
-	@skipIfConfigMismatch("incorrect_param_types")
-	def test_incorrect_param_types(self):
-		pass
+	@skipIfConfigMismatch("missing_required_params") # required params are missing from the request
+	def test_missing_required_params(self):
+		results, execution_time = self._query_endpoint()
+		# log execution time
+		for r in results:
+			self.assertEqual(r.status_code, 400)
+	
+	@skipIfUnauthenticated
+	@skipIfConfigMismatch("optional_params") # optional params are provided in the request
+	def test_optional_params(self): 
+		results, execution_time = self._query_endpoint()
+		# log execution time
+		for r in results:
+			self.assertEqual(r.status_code, 200)
+			self._check_response(r)
+	
+	@skipIfUnauthenticated
+	@skipIfConfigMismatch("undefined_param_values") # undefined (not found) param values are provided in the request
+	def test_undefined_param_values(self): 
+		results, execution_time = self._query_endpoint()
+		# log execution time
+		for r in results:
+			self.assertEqual(r.status_code, 404)
+
+	@skipIfUnauthenticated
+	@skipIfConfigMismatch("undefined_param_names") # undefined param names are given in the request
+	def test_undefined_param_names(self):
+		results, execution_time = self._query_endpoint()
+		# log execution time
+		for r in results:
+			self.assertEqual(r.status_code, 400)
+
+
+	# helper methods
+	def _query_api(self):
+		def wrap_method_call(self, params):
+			self.method_to_call(**params).execute()
+
+		start = time.time()
+		results = self.process_pool.map(wrap_method_call, self.requests_params)
+		end = time.time()
+		execution_time = end - start
+		return results, execution_time
+		
+	def _check_response(self, response):
+		for key, value in self.response_body_dict.iteritems():
+				self.assertIn(key, response.keys())
+				self.assertIs(type(response[key]), value)
+			if self.operation_type == "CREATE":
+				self.created_items.append(response[self.delete_key])
 		
 		
 def main():
 	# final report should include length of all responses and time taken for each test
 	# for each user, run the tests
 	# run single instances of each test first to ensure that they are functional
-	### run test_save_cohort (the single cohort that exists at this point should be as simple as possible)
-	### run test_cohorts_list
-	### run test_cohorts_patients_samples_list
-	### run test_patient_details
-	### run test_sample_details
-	### run test_datafilenamekey_list
-	### run test_delete_cohort (or, alternatively, just assert that the created cohort no longer exists)
-	# load/stress tests
-	### create 10 cohorts simultaneously
-	### run test_cohorts_list
-	### create 100 cohorts simultaneously
-	### run test_cohorts_list
-	### create 1000 cohorts simultaneously
-	### run test_cohorts_list
-	### create a cohort with ~10 samples
-	### run test_cohorts_patients_samples_list 
-	### run test_patient_details 
-	### run test_sample_details 
-	### run test_datafilenamekey_list
-	### run test_delete_cohort (or, alternatively, just assert that the created cohort no longer exists)
-	### create a cohort with ~100 samples
-	### run test_cohorts_patients_samples_list 
-	### run test_patient_details 
-	### run test_sample_details 
-	### run test_datafilenamekey_list
-	### run test_delete_cohort (or, alternatively, just assert that the created cohort no longer exists)
-	### create a cohort with ~1000 samples
-	### run test_cohorts_patients_samples_list 
-	### run test_patient_details 
-	### run test_sample_details 
-	### run test_datafilenamekey_list
-	### run test_delete_cohort (or, alternatively, just assert that the created cohort no longer exists)
 	suite = unittest.TestSuite()
-	suite.addTest(ParametrizedTestCase.parametrize(TestSaveCohort, config="", config_dir="", num_requests=1, auth=True))
+	suite.addTest(ParametrizedTestCase.parametrize(IsbCgcApiTest, api="cohort", config="minimal", num_requests=1, auth={}))
+	suite.addTest(ParametrizedTestCase.parametrize(IsbCgcApiTest, api="cohort", config="minimal", auth=None)
+	suite.addTest(ParametrizedTestCase.parametrize(IsbCgcApiTest, api="cohort", config="missing_required_params", num_requests=1, auth={})
+	suite.addTest(ParametrizedTestCase.parametrize(IsbCgcApiTest, api="cohort", config="optional_params", num_requests=1, auth={})
+	suite.addTest(ParametrizedTestCase.parametrize(IsbCgcApiTest, api="cohort", config="incorrect_param_types", num_requests=1, auth={})
+	suite.addTest(ParametrizedTestCase.parametrize(IsbCgcApiTest, api="cohort", config="incorrect_param_values", num_requests=1, auth={})
+	suite.addTest(ParametrizedTestCase.parametrize(IsbCgcApiTest, api="cohort", config="undefined_param_names", num_requests=1, auth={})
+	suite.addTest(ParametrizedTestCase.parametrize(IsbCgcApiTest, api="cohort", config="undefined_param_values", num_requests=1, auth={})
+
+	# load/stress tests
+	
 	
 if __name__ == "__main__":
-	main()
+	main(sys.argv[1:])
