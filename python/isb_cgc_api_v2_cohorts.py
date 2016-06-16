823c53c24c606aeb4b8003327faf2d1f99d2b6e2
medullaskyline <medullaskyline@gmail.com>
2016-06-15 19:46:54 -0700

    finished cohorts

--- python/isb_cgc_api_v2_cohorts.py
+++ python/isb_cgc_api_v2_cohorts.py
@@ -5,6 +5,7 @@ from oauth2client import tools
 from oauth2client.file import Storage
 import httplib2
 import pprint
+import json
 import sys
 import os
 
@@ -17,18 +18,20 @@ EMAIL_SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
 # where a default credentials file will be stored for use by the endpoints
 DEFAULT_STORAGE_FILE = os.path.join(os.path.expanduser("~"), '.isb_credentials')
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 # This validates the credentials of the current user against the ISB-CGC site
 
 def get_credentials():
-    oauth_flow_args = ['--noauth_local_webserver']
-    storage = Storage(DEFAULT_STORAGE_FILE)
-    credentials = storage.get()
-    if not credentials or credentials.invalid:
-        flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, EMAIL_SCOPE)
-        flow.auth_uri = flow.auth_uri.rstrip('/') + '?approval_prompt=force'
-        credentials = tools.run_flow(flow, storage, tools.argparser.parse_args(oauth_flow_args))
-    return credentials
+	oauth_flow_args = ['--noauth_local_webserver']
+	storage = Storage(DEFAULT_STORAGE_FILE)
+	credentials = storage.get()
+	if not credentials or credentials.invalid:
+		flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, EMAIL_SCOPE)
+		flow.auth_uri = flow.auth_uri.rstrip('/') + '?approval_prompt=force'
+		credentials = tools.run_flow(flow, storage, tools.argparser.parse_args(oauth_flow_args))
+	return credentials
+
 
 def get_unauthorized_service():
 	api = 'isb_cgc_api'
@@ -36,6 +39,7 @@ def get_unauthorized_service():
 	site = "http://localhost:8080"
 	discovery_url = '%s/_ah/api/discovery/v1/apis/%s/%s/rest' % (site, api, version)
 	return build(api, version, discoveryServiceUrl=discovery_url, http=httplib2.Http())
+
 
 def get_authorized_service():
 	api = 'isb_cgc_api'
@@ -53,48 +57,48 @@ def get_authorized_service():
 
 	return authorized_service
 
-def preview(service):
 
-	payload = {'age_at_initial_pathologic_diagnosis_lte': '10'}
+## resource methods
 
-	data = service.cohorts().preview(body=payload).execute()
-
-	print '\n\nresults from preview'
-	print data.get('sample_count')
-	print data.get('patient_count')
-
-def list_cohorts(service):
-
-    data = service.cohorts().list().execute()
-    print '\nresults from list_cohorts'
-    pprint.pprint(data)
-
-def create_cohort(service):
-	body = {
-		'Project': 'CCLE',
-		'Study': 'MESO'
-	}
-	data = service.cohorts().create(name='lte15v2', body=body).execute()
+def get(service, cohort_id=1, body=None, name=None):
+	data = service.cohorts().get(cohort_id=cohort_id).execute()
+	print '\nresults from cohorts().get()'
 	pprint.pprint(data)
 
-def get_cohort(service):
-	data = service.cohorts().get(cohort_id=1).execute()
-	print '\npatient_count from cohort 1: ' + data.get('patient_count')
-	return data.get('patients')[0], data.get('samples')[0]
 
-def delete_cohort(service):
-	data = service.cohorts().delete(cohort_id=9).execute()
-	print '\nresult of delete cohort'
-	print data
+def list(service, cohort_id=None, body=None, name=None):
+	data = service.cohorts().list().execute()
+	print '\nresults from cohorts().list()'
+	pprint.pprint(data)
 
-def datafilenamekeys_from_cohort(service):
-	data = service.cohorts().datafilenamekeys(cohort_id=10).execute()
-	print '\nresult of datafilenamekeys'
-	print data.get('count')
 
-def googlegenomics_from_cohort(service):
-	data = service.cohorts().googlegenomics(cohort_id=11).execute()
-	print 'result of googlegenomics'
+def preview(service, cohort_id=None, body=None, name=None):
+	data = service.cohorts().preview(body=body).execute()
+	print '\nresults from cohorts().preview()'
+	pprint.pprint(data)
+
+
+def create(service, cohort_id=None, body=None, name=None):
+	data = service.cohorts().create(name=name, body=body).execute()
+	print '\nresults from cohorts().create()'
+	pprint.pprint(data)
+
+
+def delete(service, cohort_id=None, body=None, name=None):
+	data = service.cohorts().delete(cohort_id=cohort_id).execute()
+	print '\nresults from cohorts().delete()'
+	pprint.pprint(data)
+
+
+def datafilenamekeys(service, cohort_id=None, body=None, name=None):
+	data = service.cohorts().datafilenamekeys(cohort_id=cohort_id).execute()
+	print '\nresults from cohorts().datafilenamekeys()'
+	pprint.pprint(data)
+
+
+def googlegenomics(service, cohort_id=None, body=None, name=None):
+	data = service.cohorts().googlegenomics(cohort_id=cohort_id).execute()
+	print '\nresults from cohorts().googlegenomics()'
 	pprint.pprint(data)
 
 
@@ -108,23 +112,29 @@ def main():
 						help='Id of cohort to use in get, delete, datafilenamekeys, or googlegenomics endpoints')
 	parser.add_argument('--body', '-b',
 						help='Payload to use in preview or create endpoints. Example: '
-							 '{"Study": ["BRCA", "UCS"], "age_at_initial_pathologic_diagnosis_gte": 90}',
-						default='{"Study": ["BRCA", "UCS"], "age_at_initial_pathologic_diagnosis_gte": 90}')
-	parser.add_argument('--name', 'n',
+							 '{"Study": ["BRCA", "UCS"], "age_at_initial_pathologic_diagnosis_gte": 90}')
+	parser.add_argument('--name', '-n',
 						help='The name of the cohort to create in the create endpoint.')
 	args = parser.parse_args()
 	if args.endpoint not in ['get', 'list', 'preview', 'create', 'delete', 'datafilenamekeys', 'googlegenomics']:
 		return
+
 	service = get_unauthorized_service() if args.endpoint == 'preview' else get_authorized_service()
-	# unauthorized_service = get_unauthorized_service()
-	# authorized_service = get_authorized_service()
-	# # preview(unauthorized_service)
-	# # create_cohort(authorized_service)
-	# patient_barcode, sample_barcode = get_cohort(authorized_service)
-	# delete_cohort(authorized_service)
-	# list_cohorts(authorized_service)
-	# datafilenamekeys_from_cohort(authorized_service)
-	# googlegenomics_from_cohort(authorized_service)
+	cohort_id = args.cohort_id if args.cohort_id is None else int(args.cohort_id)
+	body = json.loads(args.body) if args.body is not None else args.body
+
+	globals()[args.endpoint](service, cohort_id=cohort_id, body=body, name=args.name)
+
+
+# unauthorized_service = get_unauthorized_service()
+# authorized_service = get_authorized_service()
+# # preview(unauthorized_service)
+# # create_cohort(authorized_service)
+# patient_barcode, sample_barcode = get_cohort(authorized_service)
+# delete_cohort(authorized_service)
+# list_cohorts(authorized_service)
+# datafilenamekeys_from_cohort(authorized_service)
+# googlegenomics_from_cohort(authorized_service)
 
 
 
