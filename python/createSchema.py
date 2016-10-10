9b70a5b3c95b35920ba506822ba09e596fab1830
smrgit <sheila.reynolds@systemsbiology.org>
2016-10-10 07:05:59 -0700

    now removes special-characters from field names, and reads more of the input data file to confirm datatypes

--- python/createSchema.py
+++ python/createSchema.py
@@ -1,19 +1,19 @@
-#This script generates a JSON schema for a given data file to be used with the 'bq load' command line tool.
-#-------------------------------------------------------------
+# This script generates a JSON schema for a given data file to be used with the 'bq load' command line tool.
+# -------------------------------------------------------------
 
 import sys
 import string
 import gzip
 from dateutil.parser import parse
-#-------------------------------------------------------------
+# -------------------------------------------------------------
 
-#INPUT: path to local data file
-#OUTPUT: JSON schema to stdout
-#BigQuery data types = ['string','bytes','integer','float','boolean','record','timestamp']
-#BigQuery modes = ['nullable','required','repeated'] , default is nullable
-#-------------------------------------------------------------
+# INPUT: path to local data file
+# OUTPUT: JSON schema to stdout
+# BigQuery data types = ['string','bytes','integer','float','boolean','record','timestamp']
+# BigQuery modes = ['nullable','required','repeated'] , default is nullable
+# -------------------------------------------------------------
 
-#function to check is a given value is numeric
+# function to check is a given value is numeric
 def isNumeric(val):
     try:
         float(val)
@@ -21,102 +21,199 @@ def isNumeric(val):
     except ValueError:
         return False
 
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 
-#open data file to read header and 1st data row to infer data types of columns
+specialChars = [ ' ', '-', ')', '(', ',', ':', ';', '.', '@', '#', '$', '%', '^', '&', '*', '[', ']', '{', '}', '|' ]
+
+def removeSpecialChars ( aString ):
+
+    bString = ''
+    for ii in range(len(aString)):
+        if ( aString[ii] in specialChars ):
+            bString += '_'
+        else:
+            bString += aString[ii]
+
+    return ( bString )
+
+# --------------------------------------------------------------
+
+def inferDataTypes ( dataRow, dataTypes ):
+
+    for ii in range(len(dataRow)):
+
+        item = dataRow[ii].strip()
+
+        if ( item == '' ):
+            ## print " SKIPPING field #%d because it is blank ... " % ii      
+            continue
+
+        elif ( dataTypes[ii] == "string" ):
+            ## print " SKIPPING field #%d because it is already a STRING " % ii
+            continue
+
+        elif ( item.lower()=="true" or item.lower()=="false" ):
+            if ( dataTypes[ii] == "NA" ):
+                print " initially setting field #%d to BOOLEAN (%s) " % ( ii, item )
+                dataTypes[ii] = "boolean"
+            elif ( dataTypes[ii] == "boolean" ):
+                continue
+            else: 
+                print " ERROR ??? conflicting data types ??? ", item, dataTypes[ii]
+                dataTypes[ii] = "string"
+
+        else:
+
+            try:
+                iVal = int(item)
+                if ( dataTypes[ii] == "NA" ):
+                    print " initially setting field #%d to INTEGER (%s) " % ( ii, item )
+                    dataTypes[ii] = "integer"
+                elif ( dataTypes[ii] == "integer" ):
+                    continue
+                elif ( dataTypes[ii] == "float" ):
+                    continue
+                else:
+                    print " ERROR ??? conflicting data types ??? ", item, dataTypes[ii]
+                    dataTypes[ii] = "string"
+
+            except:
+                try:
+                    fVal = float(item)
+                    if ( dataTypes[ii] == "NA" ):
+                        print " initially setting field #%d to FLOAT (%s) " % ( ii, item )
+                        dataTypes[ii] = "float"
+                    elif ( dataTypes[ii] == "float" ):
+                        continue
+                    elif ( dataTypes[ii] == "integer" ):
+                        print " CHANGING field #%d from INTEGER to FLOAT (%s) " % ( ii, item )
+                        dataTypes[ii] = "float"
+                        continue
+                    else:
+                        print " ERROR ??? conflicting data types ??? ", item, dataTypes[ii]
+                        dataTypes[ii] = "string"
+
+                except:
+                    if ( dataTypes[ii] == "NA" ):
+                        print " initially setting field #%d to STRING (%s) " % ( ii, item )
+                    else:
+                        print " CHANGING field #%d to STRING (%s) " % ( ii, item )
+                    dataTypes[ii] = "string"
+
+    ## print dataTypes
+    return ( dataTypes )
+        
+# --------------------------------------------------------------
+
+inFilename = sys.argv[1]
+
+# open data file ...
 try:
-    if sys.argv[1].endswith('gz'):
-        dataFile = gzip.open(sys.argv[1],"r")
+    if inFilename.endswith('gz'):
+        dataFile = gzip.open(inFilename,"r")
     else:
-        dataFile = open(sys.argv[1],"r")
+        dataFile = open(inFilename,"r")
 except:
     print 'requires input filename as command-line parameter'
     sys.exit()
 
 print " "
-print "Parsing input file <%s>." % sys.argv[1]
+print "Parsing input file <%s>." % inFilename
 print " "
 
-#first line is expected to be the header
-expectedHeader = dataFile.readline().strip().split('\t')
+# first line is expected to be the header
+headerRow = dataFile.readline().split('\t')
 
-#if any numeric values in this first line, it is likely not a header: hence exit
-if any([isNumeric(x) for x in expectedHeader]):
+# if any numeric values in this first line, it is likely not a header: hence exit
+if any([isNumeric(x) for x in headerRow]):
     print 'Numeric fields found in the first line. Perhaps the header is missing. Please check input file.'
-    print expectedHeader
+    print headerRow
     sys.exit()
 
-#if any of the header tokens are not unique, that's a problem ...
-for ii in range(len(expectedHeader)):
-    for jj in range(ii+1,len(expectedHeader)):
-        if ( expectedHeader[ii].lower() == expectedHeader[jj].lower() ):
-            print "ERROR: repeated header tokens.  Please check input file."
-            print expectedHeader
-            sys.exit(-1)
-        if ( expectedHeader[ii] == '' ):
-            print "ERROR: blank header token.  Please check input file."
-            print expectedHeader
-            sys.exit(-1)
-
-#else read the first data row to infer column data types
-firstDataRow = dataFile.readline().strip().split('\t')
-
-if ( len(expectedHeader) != len(firstDataRow) ):
-    print "The number of tab-separated tokens in the first row do not match? Please check file. "
-    print len(expectedHeader), len(firstDataRow)
-else:
-    print "%d columns found in input file." % len(firstDataRow)
+# build up a list of field names based on the header tokens and make sure they
+# are all unique
+fieldNames = []
+lowerNames = []
+for ii in range(len(headerRow)):
+    aName = removeSpecialChars ( headerRow[ii].strip() )
+    if ( aName.lower() in lowerNames ):
+        print " ERROR: repeated header token <%s> " % aName
+        sys.exit(-1)
+    elif ( aName == "" ):
+        print " ERROR: blank header token ??? "
+        sys.exit(-1)
+    else:
+        fieldNames += [ aName ]
+        lowerNames += [ aName.lower() ]
 
 print " "
+print fieldNames
 print " "
-print "draft JSON schema : "
-print " "
 
-#print opening bracket
-print '['
+dataTypes = ['NA' ] * len(fieldNames)
 
-# the available data types are described in detail at: https://cloud.google.com/bigquery/data-types
-# and include: STRING, BYTES, INTEGER, FLOAT, BOOLEAN ('true' or 'false'),
-# RECORD, and TIMESTAMP
-# here we will only try to infer STRING, INTEGER, FLOAT, or BOOLEAN
+## this is the # of lines that we'll skip over each time we
+## read and parse a single line of data ...
+nSkip = 1000
 
-#loop through the columns
-for index,item in enumerate(firstDataRow):
+done = 0
+while not done:
 
-    # if we have a blank field, we can't really infer anything
-    # so we'll assume string and move on ...
-    if ( item == '' ):
-        outStr = '    {"name": "'+expectedHeader[index]+'", "type": "string", "mode": "nullable"}'
+    # next, read a data row to infer column data types
+    dataRow = dataFile.readline().split('\t')
+    if ( len(dataRow) == 1 ):
+        done = 1
         continue
 
-    # the first test is for boolean
-    if ( item.lower()=="true" or item.lower()=="false" ):
-        outStr = '    {"name": "'+expectedHeader[index]+'", "type": "boolean", "mode": "nullable"}'
-    else:
-        try:
-            # next try to cast it as an integer
-            iVal = int(item)
-            outStr = '    {"name": "'+expectedHeader[index]+'", "type": "integer", "mode": "nullable"}'
-        except:
-            try:
-                # or a float ...
-                fVal = float(item)
-                outStr = '    {"name": "'+expectedHeader[index]+'", "type": "float", "mode": "nullable"}'
-            except:
-                try:
-                    # use the dateutil parser to see if it looks like a date
-                    parse(item)
-                    outStr = '    {"name": "'+expectedHeader[index]+'", "type": "timestamp", "mode": "nullable"}'
-                except:
-                    # final catch-all is string type
-                    outStr = '    {"name": "'+expectedHeader[index]+'", "type": "string", "mode": "nullable"}'
-            
-    if index < len(firstDataRow)-1:
+    if ( len(dataRow) != len(fieldNames) ):
+        print " ERROR ??? # of values in data row is not as expected ??? ", len(dataRow), len(fieldNames)
+        print " "
+        for ii in range(min(len(dataRow),len(fieldNames))):
+            print " %3d  %s  %s " % ( ii, fieldNames[ii], dataRow[ii] )
+        sys.exit(-1)
+
+    dataTypes = inferDataTypes ( dataRow, dataTypes )
+
+    ## skip over a bunch of rows, we don't want to check every single row,
+    ## just a few of them at random ...
+    for jj in range(nSkip):
+        dataRow = dataFile.readline()
+        if ( len(dataRow) < 1 ): done = 1
+
+dataFile.close()
+
+schemaFilename = inFilename + ".json"
+try:
+    fhOut = file ( schemaFilename, 'w' )
+except:
+    print " ERROR??? failed to open output schema file??? "
+    print schemaFilename
+    sys.exit(-1)
+
+print " "
+print " "
+print "writing draft JSON schema to <%s> " % schemaFilename
+print " "
+
+# print opening bracket
+fhOut.write ( '[\n' )
+
+#  the available data types are described in detail at: https://cloud.google.com/bigquery/data-types
+#  and include: STRING, BYTES, INTEGER, FLOAT, BOOLEAN ('true' or 'false'),
+#  RECORD, and TIMESTAMP
+#  here we will only try to infer STRING, INTEGER, FLOAT, or BOOLEAN
+
+# loop through the columns
+for ii in range(len(fieldNames)):
+
+    outStr = '    {"name": "'+fieldNames[ii]+'", "type": "'+dataTypes[ii]+'", "mode": "nullable", "description": "<add description here>"}'
+    if ( ii < len(fieldNames)-1 ):
         outStr+=','
 
-    print outStr
+    fhOut.write ( '%s\n' % outStr )
 
-#print closing bracket
-print ']'
+# print closing bracket
+fhOut.write ( ']\n' )
 
+fhOut.close()
 
