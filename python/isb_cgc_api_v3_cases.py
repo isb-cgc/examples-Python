1470549e46fb3395626f9e7459189be1b9dca20d
Michael Miller <mmiller@systemsbiology.org>
2017-05-22 11:44:34 -0700

    update code for v3 APIs

--- python/isb_cgc_api_v3_cases.py
+++ python/isb_cgc_api_v3_cases.py
@@ -1,3 +1,18 @@
+'''
+Copyright 2017, Institute for Systems Biology.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 from argparse import ArgumentParser
 from googleapiclient.discovery import build
 from oauth2client.client import OAuth2WebServerFlow
@@ -30,30 +45,28 @@ def get_credentials():
     return credentials
 
 def get_unauthorized_service():
-	api = 'isb_cgc_api'
-	version = 'v2'
-	site = "https://api-dot-isb-cgc.appspot.com"
-	discovery_url = '%s/_ah/api/discovery/v1/apis/%s/%s/rest' % (site, api, version)
-	return build(api, version, discoveryServiceUrl=discovery_url, http=httplib2.Http())
+    api = 'isb_cgc_tcga_api'
+    version = 'v3'
+    site = "https://api-dot-isb-cgc.appspot.com"
+    discovery_url = '%s/_ah/api/discovery/v1/apis/%s/%s/rest' % (site, api, version)
+    return build(api, version, discoveryServiceUrl=discovery_url, http=httplib2.Http())
 
-
+# the API uses the isb_cgc_tcga_api endpoint but is also part of the isb_cgc_ccle_api and isb_cgc_target_api endpoints
 def get(service, barcode):
-	"""
-	Usage: python python/isb_cgc_api_v2_patients.py -b TCGA-W5-AA2R
-	"""
-	data = service.patients().get(patient_barcode=barcode).execute()
-	print '\nResults from patients().get()'
-	pprint.pprint(data)
-
+    """
+    Usage: python python/isb_cgc_api_v3_cases.py -b TCGA-W5-AA2R
+    """
+    data = service.patients().get(case_barcode=barcode).execute()
+    print '\nResults from patients().get()'
+    pprint.pprint(data)
 
 def main():
-	parser = ArgumentParser()
-	parser.add_argument('--barcode', '-b',
-						help='Patient barcode. Example: TCGA-W5-AA2R')
-	args = parser.parse_args()
-	service = get_unauthorized_service()
-	get(service, args.barcode)
-
+    parser = ArgumentParser()
+    parser.add_argument('--barcode', '-b',
+                        help='Case barcode. Example: TCGA-W5-AA2R')
+    args = parser.parse_args()
+    service = get_unauthorized_service()
+    get(service, args.barcode)
 
 if __name__ == '__main__':
-	main()
+    main()
--- python/isb_cgc_api_v3_samples.py
+++ python/isb_cgc_api_v3_samples.py
@@ -1,3 +1,18 @@
+'''
+Copyright 2017, Institute for Systems Biology.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 from argparse import ArgumentParser
 from googleapiclient.discovery import build
 from oauth2client.client import OAuth2WebServerFlow
@@ -30,52 +45,42 @@ def get_credentials():
     return credentials
 
 def get_unauthorized_service():
-	api = 'isb_cgc_api'
-	version = 'v2'
-	site = "https://api-dot-isb-cgc.appspot.com"
-	discovery_url = '%s/_ah/api/discovery/v1/apis/%s/%s/rest' % (site, api, version)
-	return build(api, version, discoveryServiceUrl=discovery_url, http=httplib2.Http())
+    api = 'isb_cgc_tcga_api'
+    version = 'v3'
+    site = "https://api-dot-isb-cgc.appspot.com"
+    discovery_url = '%s/_ah/api/discovery/v1/apis/%s/%s/rest' % (site, api, version)
+    return build(api, version, discoveryServiceUrl=discovery_url, http=httplib2.Http())
 
-
+# the APIs use the isb_cgc_tcga_api endpoint but are also part of the isb_cgc_ccle_api and isb_cgc_target_api endpoints
 def get(service, barcode):
-	"""
-	Usage: python python/isb_cgc_api_v2_samples.py -e get -b TCGA-W5-AA2R-01A
-	"""
-	data = service.samples().get(sample_barcode=barcode).execute()
-	print '\nresults from samples().get()'
-	pprint.pprint(data)
+    """
+    Usage: python python/isb_cgc_api_v3_samples.py -e get -b TCGA-W5-AA2R-01A
+    """
+    data = service.samples().get(sample_barcode=barcode).execute()
+    print '\nresults from samples().get()'
+    pprint.pprint(data)
 
 def cloud_storage_file_paths(service, barcode=None):
-	"""
-	Usage: python python/isb_cgc_api_v2_samples.py -e cloud_storage_file_paths -b CCLE-ACC-MESO-1-DNA-08
-	"""
-	data = service.samples().cloud_storage_file_paths(sample_barcode=barcode).execute()
-	print '\nresults from samples().cloud_storage_file_paths()'
-	pprint.pprint(data)
-
-def googlegenomics(service, barcode=None):
-	"""
-	Usage: python python/isb_cgc_api_v2_samples.py -e googlegenomics -b CCLE-ACC-MESO-1-DNA-08
-	"""
-	data = service.samples().googlegenomics(sample_barcode=barcode).execute()
-	print '\nresults from samples().googlegenomics()'
-	pprint.pprint(data)
-
+    """
+    Usage: python python/isb_cgc_api_v3_samples.py -e cloud_storage_file_paths -b TCGA-01-0642-11A
+    """
+    data = service.samples().cloud_storage_file_paths(sample_barcode=barcode).execute()
+    print '\nresults from samples().cloud_storage_file_paths()'
+    pprint.pprint(data)
 
 def main():
-	parser = ArgumentParser()
-	parser.add_argument('--endpoint', '-e',
-						help='Name of cohorts endpoint to execute. '
-							 'Options: get, cloud_storage_file_paths, googlegenomics')
-	parser.add_argument('--barcode', '-b',
-						help='Sample barcode. Examples: TCGA-W5-AA2R-01A, CCLE-ACC-MESO-1-DNA-08')
-	args = parser.parse_args()
-	if args.endpoint not in ['get', 'cloud_storage_file_paths', 'googlegenomics']:
-		return
+    parser = ArgumentParser()
+    parser.add_argument('--endpoint', '-e',
+                        help='Name of samples endpoint to execute. '
+                             'Options: get, cloud_storage_file_paths')
+    parser.add_argument('--barcode', '-b',
+                        help='Sample barcode. Examples: TCGA-W5-AA2R-01A, TCGA-01-0642-11A')
+    args = parser.parse_args()
+    if args.endpoint not in ['get', 'cloud_storage_file_paths']:
+        return
 
-	service = get_unauthorized_service()
-	globals()[args.endpoint](service, barcode=args.barcode)
-
+    service = get_unauthorized_service()
+    globals()[args.endpoint](service, barcode=args.barcode)
 
 if __name__ == '__main__':
-	main()
+    main()
