ff73487f3ac549f24208eb891b0abdf5f4acb3fa
smrgit <sheila.reynolds@systemsbiology.org>
2018-03-08 10:19:28 -0800

    updated, added more helpful usage & error info

--- python/isb_cgc_api_v3_cases.py
+++ python/isb_cgc_api_v3_cases.py
@@ -1,5 +1,7 @@
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
 '''
-Copyright 2017, Institute for Systems Biology.
+Copyright 2018, Institute for Systems Biology.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,11 +15,18 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
+
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
 from argparse import ArgumentParser
 from googleapiclient.discovery import build
+
 import httplib2
-import pprint
 import os
+import pprint
+import sys
+
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
 # the CLIENT_ID for the ISB-CGC site
 CLIENT_ID = '907668440978-0ol0griu70qkeb6k3gnn2vipfa5mgl60.apps.googleusercontent.com'
@@ -28,6 +37,8 @@ EMAIL_SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
 # where a default credentials file will be stored for use by the endpoints
 DEFAULT_STORAGE_FILE = os.path.join(os.path.expanduser("~"), '.isb_credentials')
 
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
 def get_unauthorized_service():
     api = 'isb_cgc_tcga_api'
     version = 'v3'
@@ -35,22 +46,46 @@ def get_unauthorized_service():
     discovery_url = '%s/_ah/api/discovery/v1/apis/%s/%s/rest' % (site, api, version)
     return build(api, version, discoveryServiceUrl=discovery_url, http=httplib2.Http())
 
-# the API uses the isb_cgc_tcga_api endpoint but is also part of the isb_cgc_ccle_api and isb_cgc_target_api endpoints
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+# the example uses the TCGA-specific endpoint but the same functionality
+# exists in the TARGET and CCLE endpoints as well
+
 def get(service, barcode):
     """
-    Usage: python python/isb_cgc_api_v3_cases.py -b TCGA-W5-AA2R
+    Usage: python isb_cgc_api_v3_cases.py -b TCGA-W5-AA2R
     """
     data = service.cases().get(case_barcode=barcode).execute()
     print '\nResults from cases().get()'
     pprint.pprint(data)
 
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+def print_usage():
+    print " "
+    print " Usage: python %s -b <case barcode> " % sys.argv[0]
+    print " "
+    print " Example: python %s -b TCGA-W5-AA2R " % sys.argv[0]
+    print " "
+
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('--barcode', '-b', type=str, required=True, 
                         action='store', help='Case barcode. Example: TCGA-W5-AA2R')
-    args = parser.parse_args()
+    try:
+        args = parser.parse_args()
+    except:
+        print_usage()
+        return
+
     service = get_unauthorized_service()
     get(service, args.barcode)
 
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
 if __name__ == '__main__':
     main()
+
+# -#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
