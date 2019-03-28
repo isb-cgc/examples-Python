9e0af6c571142744c6b2eddf634056e379638ef6
smrgit <sheila.reynolds@systemsbiology.org>
2019-03-28 11:53:22 -0700

    major modifications to be able to infer data types AND modes...

--- python/createSchema.py
+++ python/createSchema.py
@@ -49,7 +49,9 @@ def removeSpecialChars ( aString ):
     except:
         doNothing = 1
 
-    print "     removeSpecialChars : <%s> <%s> " % ( aString, bString )
+    if ( bString != aString ):
+        print "     removeSpecialChars : <%s> <%s> " % ( aString, bString )
+
     return ( bString )
 
 # --------------------------------------------------------------
@@ -93,7 +95,7 @@ def createValidBQfieldName ( aString ):
         cString = bString
 
     ## check first character:
-    print " <%s> " % cString
+    ## print " <%s> " % cString
     try:
         if not letter_or_underscore ( cString[0] ):
             print " createValidBQfieldName: first character is not valid <%s> " % cString
@@ -111,93 +113,145 @@ def createValidBQfieldName ( aString ):
 
 # --------------------------------------------------------------
 
-def inferDataTypes ( dataRow, dataTypes, fieldNames ):
+def splitListString ( aString ):
+
+    ## print " in splitListString : <%s> " % aString
+
+    aTokens = []
+    if ( aString.startswith("u'") ):
+        ii = 2
+        while ( ii < len(aString) ):
+            jj = aString.find("'",ii)
+            if ( jj > ii ):
+                aTokens += [ aString[ii:jj] ]
+                ii = jj
+            ii = aString.find("'",jj+1)
+            if ( ii < 0 ): ii = len(aString)
+
+    else:
+        aTokens = aString.split(',')
+
+    return ( aTokens )
+
+# --------------------------------------------------------------
+# this function infers the "types" and "modes" for each
+# of the input fields, based on a single input dataRow
+
+def inferDataTM ( dataRow, dataTypes, dataModes, fieldNames ):
 
     for ii in range(len(dataRow)):
 
         item = dataRow[ii].strip()
+        if ( len(item) < 1 ): continue
 
-        if ( item == '' or item == 'NA'):
-            ## print " SKIPPING field #%d because it is blank ... " % ii      
-            continue
-
-        elif ( dataTypes[ii] == "string" ):
-            ## print " SKIPPING field #%d because it is already a STRING " % ii
-            continue
-
-        elif ( item.lower()=="true" or item.lower()=="false" ):
-            if ( dataTypes[ii] == "NA" ):
-                print " initially setting field #%d (%s) to BOOLEAN (%s) " % ( ii, fieldNames[ii], item )
-                dataTypes[ii] = "boolean"
-            elif ( dataTypes[ii] == "boolean" ):
-                continue
-            else: 
-                print " ERROR ??? conflicting data types ??? ", item, dataTypes[ii]
-                dataTypes[ii] = "string"
+        if ( item[0] == '[' and item[-1] == ']' ):
+            ## print ii, item
+            ## print " this item looks like a LIST !!! "
+            aList = item[1:-1]
+            aToks = splitListString ( aList )
+            if ( len(aToks) > 0 ):
+                if ( dataModes[ii] == 'NA' ):
+                    print " initially setting field #%d (%s) mode to REPEATED " % ( ii, fieldNames[ii] )
+                    dataModes[ii] = "repeated"
+                elif ( dataModes[ii] == 'nullable' ):
+                    print " CHANGING field #%d (%s) mode to REPEATED " % ( ii, fieldNames[ii] )
+                    dataModes[ii] = "repeated"
 
         else:
+            aToks = [ item ]
+            if ( dataModes[ii] == 'NA' ): 
+                print " initially setting field #%d (%s) mode to NULLABLE " % ( ii, fieldNames[ii] )
+                dataModes[ii] = "nullable"
 
-            try:
-                iVal = int(item)
+        for jtem in aToks:
+
+            if ( jtem == '' or jtem == 'NA'):
+                ## print " SKIPPING field #%d because it is blank ... " % ii      
+                continue
+    
+            elif ( dataTypes[ii] == "string" ):
+                ## print " SKIPPING field #%d because it is already a STRING " % ii
+                continue
+    
+            elif ( jtem.lower()=="true" or jtem.lower()=="false" ):
                 if ( dataTypes[ii] == "NA" ):
-                    print " initially setting field #%d (%s) to INTEGER (%s) " % ( ii, fieldNames[ii], item )
-                    dataTypes[ii] = "integer"
-                elif ( dataTypes[ii] == "integer" ):
+                    print " initially setting field #%d (%s) type to BOOLEAN (%s) " % ( ii, fieldNames[ii], jtem )
+                    dataTypes[ii] = "boolean"
+                elif ( dataTypes[ii] == "boolean" ):
                     continue
-                elif ( dataTypes[ii] == "float" ):
-                    continue
-                else:
-                    print " ERROR ??? conflicting data types ??? ", item, dataTypes[ii]
+                else: 
+                    print " ERROR ??? conflicting data types ??? ", jtem, dataTypes[ii]
                     dataTypes[ii] = "string"
-
-            except:
+    
+            else:
+    
                 try:
-                    fVal = float(item)
+                    iVal = int(jtem)
                     if ( dataTypes[ii] == "NA" ):
-                        print " initially setting field #%d (%s) to FLOAT (%s) " % ( ii, fieldNames[ii], item )
-                        dataTypes[ii] = "float"
+                        print " initially setting field #%d (%s) type to INTEGER (%s) " % ( ii, fieldNames[ii], jtem )
+                        dataTypes[ii] = "integer"
+                    elif ( dataTypes[ii] == "integer" ):
+                        continue
                     elif ( dataTypes[ii] == "float" ):
                         continue
-                    elif ( dataTypes[ii] == "integer" ):
-                        print " CHANGING field #%d (%s) from INTEGER to FLOAT (%s) " % ( ii, fieldNames[ii], item )
-                        dataTypes[ii] = "float"
-                        continue
                     else:
-                        print " ERROR ??? conflicting data types ??? ", item, dataTypes[ii]
+                        print " ERROR ??? conflicting data types ??? ", jtem, dataTypes[ii]
+                        dataTypes[ii] = "string"
+    
+                except:
+                    try:
+                        fVal = float(jtem)
+                        if ( dataTypes[ii] == "NA" ):
+                            print " initially setting field #%d (%s) type to FLOAT (%s) " % ( ii, fieldNames[ii], jtem )
+                            dataTypes[ii] = "float"
+                        elif ( dataTypes[ii] == "float" ):
+                            continue
+                        elif ( dataTypes[ii] == "integer" ):
+                            print " CHANGING field #%d (%s) from INTEGER to FLOAT (%s) " % ( ii, fieldNames[ii], jtem )
+                            dataTypes[ii] = "float"
+                            continue
+                        else:
+                            print " ERROR ??? conflicting data types ??? ", jtem, dataTypes[ii]
+                            dataTypes[ii] = "string"
+    
+                    except:
+                        if ( dataTypes[ii] == "NA" ):
+                            print " initially setting field #%d (%s) type to STRING (%s) " % ( ii, fieldNames[ii], jtem )
+                        else:
+                            print " CHANGING field #%d (%s) to STRING (%s) " % ( ii, fieldNames[ii], jtem )
                         dataTypes[ii] = "string"
 
-                except:
-                    if ( dataTypes[ii] == "NA" ):
-                        print " initially setting field #%d (%s) to STRING (%s) " % ( ii, fieldNames[ii], item )
-                    else:
-                        print " CHANGING field #%d (%s) to STRING (%s) " % ( ii, fieldNames[ii], item )
-                    dataTypes[ii] = "string"
-
     ## print dataTypes
-    return ( dataTypes )
+    return ( dataTypes, dataModes )
         
 # --------------------------------------------------------------
 
+# TODO: clean up this code ... also make it capable of handling
+# both TSVs and CSVs ... and look at the shelx module/class 
+
 if ( len(sys.argv) == 1 ):
     print " "
-    print " Usage : %s <input-filename> <nSkip> "
-    print "         where nSkip specifies the # of lines skipped between "
-    print "         lines that are parsed and checked for data-types; "
-    print "         if the input file is small, you can leave set nSkip "
-    print "         to be small, but if the input file is very large, nSkip "
-    print "         should probably be 1000 or more (default value is 1000) "
+    print " Usage : %s <input-filename> <nSkip> <topSkip> "
+    print "       * nSkip: # of lines skipped between lines that are parsed and checked for data-types; "
+    print "         if the input file is small, you can leave set nSkip to be small, but if the input "
+    print "         file is very large, nSkip should probably be 1000 or more (default value is 1000) "
+    print "       * topSkip: # of lines to be completely skipped at the top of the file (default 0) "
     print " "
     sys.exit(-1)
 
 inFilename = sys.argv[1]
 
-
 ## this is the # of lines that we'll skip over each time we
 ## read and parse a single line of data ...
 nSkip = 1000
-if ( len(sys.argv) == 3 ):
+if ( len(sys.argv) >= 3 ):
     nSkip = int ( sys.argv[2] )
     if ( nSkip < 0 ): nSkip = 0
+
+topSkip = 0
+if ( len(sys.argv) >= 4 ):
+    topSkip = int ( sys.argv[3] )
+    if ( topSkip < 0 ): topSkip = 0
 
 ## scratch file ...
 dmpFh = file ( "subsample.tsv", 'w' )
@@ -217,6 +271,11 @@ except:
 print " "
 print "Parsing input file <%s>." % inFilename
 print " "
+
+# start by skipping the specified 'topSkip' lines ...
+for i in range(topSkip):
+    aLine = dataFile.readline()
+    print " skipping line %d <%s...> " % ( (i+1), aLine[:16] )
 
 # first line is expected to be the header
 aLine = dataFile.readline()
@@ -254,7 +313,8 @@ print " "
 print fieldNames
 print " "
 
-dataTypes = ['NA' ] * len(fieldNames)
+dataTypes = ['NA'] * len(fieldNames)
+dataModes = ['NA'] * len(fieldNames)
 
 
 done = 0
@@ -276,7 +336,7 @@ while not done:
             print " %3d  %s  %s " % ( ii, fieldNames[ii], dataRow[ii] )
         sys.exit(-1)
 
-    dataTypes = inferDataTypes ( dataRow, dataTypes, fieldNames )
+    ( dataTypes, dataModes ) = inferDataTM ( dataRow, dataTypes, dataModes, fieldNames )
 
     ## skip over a bunch of rows, we don't want to check every single row,
     ## just a few of them at random ...
@@ -312,10 +372,10 @@ fhOut.write ( '[\n' )
 for ii in range(len(fieldNames)):
 
     # in case we got this far w/o a dataType getting set ...
-    if ( dataTypes[ii] == "NA" ):
-        dataTypes[ii] = "string"
+    if ( dataTypes[ii] == "NA" ): dataTypes[ii] = "string"
+    if ( dataModes[ii] == "NA" ): dataModes[ii] = "nullable"
 
-    outStr = '    {"name": "'+fieldNames[ii]+'", "type": "'+dataTypes[ii]+'", "mode": "nullable", "description": "<add description here>"}'
+    outStr = '    {"name": "'+fieldNames[ii]+'", "type": "'+dataTypes[ii]+'", "mode": "'+dataModes[ii]+'", "description": "<add description here>"}'
     if ( ii < len(fieldNames)-1 ):
         outStr+=','
 
